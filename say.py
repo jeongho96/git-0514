a4041a96dc34332b6d6e505ecd85e03aa8081f1c
jeongho96 <jamiserlee@naver.com>
2022-05-14 13:11:29 +0900

    Edit say.py - for conflict test

--- say.py
+++ say.py
@@ -1,6 +1,6 @@
 def hi(name):
-    print("hihi--main", name)
+    print("hihi~~conflict^^^^", name)
 
-if __name__ == "__main__":
-    print("This is say.py - main")
+if "__name__" == "__main__":
+    print("This is say.py")
     hi('jamong')
